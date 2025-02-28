0152408f2b028449ddac2962399986e6ff5ffd59
Artem Iarukov <ppjisd@gmail.com>
2025-03-01 00:22:05 +0300

    Все готово

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -3,6 +3,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, CallbackQuery, FSInputFile
 from sqlalchemy.ext.asyncio import AsyncSession
+from aiogram.types import InputMediaPhoto  # Для создания медиагруппы
 
 from database.orm_query import orm_get_glosses_balms, orm_get_balm_in_stick, orm_get_balm_in_iron, orm_get_mask_for_lip
 from filters.chat_types import ChatTypeFilter
@@ -24,6 +25,7 @@ from keyboards.start import start_keyboard
 router = Router()
 router.message.filter(ChatTypeFilter(["private"]))
 
+
 class Questionnaire(StatesGroup):
     dry_lips = State()
     cracked_lips = State()
@@ -34,17 +36,17 @@ class Questionnaire(StatesGroup):
     smoke = State()
     allergies = State()
     format_product = State()
+    color_product = State()  # Новое состояние для выбора цвета
     aromat = State()
 
 
 welcome_text = (
-        "Давай выберем формат твоего продукта"
-    )
+    "Давай выберем формат твоего продукта"
+)
 
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
-
     file_path = "GUID.pdf"
     # Отправляем файл
     await message.answer_document(
@@ -54,11 +56,11 @@ async def cmd_start(message: Message):
     await message.answer(welcome_text, reply_markup=start_keyboard())
 
 
-
 @router.callback_query(lambda query: query.data == "unique_product")
 async def start_questionnaire(query: CallbackQuery, state: FSMContext):
     await query.message.edit_text("Испытываете ли вы сухость губ?", reply_markup=yes_no_back_keyboard())
     await state.set_state(Questionnaire.dry_lips)
+
 
 @router.callback_query(Questionnaire.dry_lips)
 async def process_dry_lips(query: CallbackQuery, state: FSMContext):
@@ -70,6 +72,7 @@ async def process_dry_lips(query: CallbackQuery, state: FSMContext):
     await query.message.edit_text("Трескаются ли у вас губы?", reply_markup=yes_no_back_keyboard())
     await state.set_state(Questionnaire.cracked_lips)
 
+
 @router.callback_query(Questionnaire.cracked_lips)
 async def process_cracked_lips(query: CallbackQuery, state: FSMContext):
     if query.data == "back":
@@ -77,8 +80,10 @@ async def process_cracked_lips(query: CallbackQuery, state: FSMContext):
         await state.set_state(Questionnaire.dry_lips)
         return
     await state.update_data(cracked_lips=query.data)
-    await query.message.edit_text("Часто ли у вас воспаляется кожа на губах и в уголках губ?", reply_markup=yes_no_back_keyboard())
+    await query.message.edit_text("Часто ли у вас воспаляется кожа на губах и в уголках губ?",
+                                  reply_markup=yes_no_back_keyboard())
     await state.set_state(Questionnaire.inflamed_lips)
+
 
 @router.callback_query(Questionnaire.inflamed_lips)
 async def process_inflamed_lips(query: CallbackQuery, state: FSMContext):
@@ -90,15 +95,18 @@ async def process_inflamed_lips(query: CallbackQuery, state: FSMContext):
     await query.message.edit_text("Присутствует ли у вас на губах шелушения?", reply_markup=yes_no_back_keyboard())
     await state.set_state(Questionnaire.peeling_lips)
 
+
 @router.callback_query(Questionnaire.peeling_lips)
 async def process_peeling_lips(query: CallbackQuery, state: FSMContext):
     if query.data == "back":
-        await query.message.edit_text("Часто ли у вас воспаляется кожа на губах и в уголках губ?", reply_markup=yes_no_back_keyboard())
+        await query.message.edit_text("Часто ли у вас воспаляется кожа на губах и в уголках губ?",
+                                      reply_markup=yes_no_back_keyboard())
         await state.set_state(Questionnaire.inflamed_lips)
         return
     await state.update_data(peeling_lips=query.data)
     await query.message.edit_text("Обветриваются ли у вас губы?", reply_markup=yes_no_back_keyboard())
     await state.set_state(Questionnaire.chapped_lips)
+
 
 @router.callback_query(Questionnaire.chapped_lips)
 async def process_chapped_lips(query: CallbackQuery, state: FSMContext):
@@ -110,6 +118,7 @@ async def process_chapped_lips(query: CallbackQuery, state: FSMContext):
     await query.message.edit_text("Кусаете ли вы губы?", reply_markup=yes_no_back_keyboard())
     await state.set_state(Questionnaire.bite_lips)
 
+
 @router.callback_query(Questionnaire.bite_lips)
 async def process_bite_lips(query: CallbackQuery, state: FSMContext):
     if query.data == "back":
@@ -119,6 +128,7 @@ async def process_bite_lips(query: CallbackQuery, state: FSMContext):
     await state.update_data(bite_lips=query.data)
     await query.message.edit_text("Курите ли вы?", reply_markup=yes_no_back_keyboard())
     await state.set_state(Questionnaire.smoke)
+
 
 @router.callback_query(Questionnaire.smoke)
 async def process_smoke(query: CallbackQuery, state: FSMContext):
@@ -130,6 +140,7 @@ async def process_smoke(query: CallbackQuery, state: FSMContext):
     await query.message.edit_text("Есть ли у вас аллергия?", reply_markup=yes_no_back_keyboard())
     await state.set_state(Questionnaire.allergies)
 
+
 @router.callback_query(Questionnaire.allergies)
 async def process_allergies(query: CallbackQuery, state: FSMContext):
     if query.data == "back":
@@ -139,28 +150,80 @@ async def process_allergies(query: CallbackQuery, state: FSMContext):
     await state.update_data(allergies=query.data)
     await query.message.edit_text("Теперь выберем формат продукта", reply_markup=format_product_kb())
     await state.set_state(Questionnaire.format_product)
+def color_keyboard():
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="01", callback_data="color_01")],
+        [InlineKeyboardButton(text="02", callback_data="color_02")],
+        [InlineKeyboardButton(text="03", callback_data="color_03")],
+        [InlineKeyboardButton(text="Назад", callback_data="back")]
+    ])
+    return keyboard
+
 
 @router.callback_query(Questionnaire.format_product)
-async def process_allergies(query: CallbackQuery, state: FSMContext):
+async def process_format_product(query: CallbackQuery, state: FSMContext):
     if query.data == "back":
-        await query.message.edit_text("Есть ли у вас аллергия", reply_markup=yes_no_back_keyboard())
+        await query.message.edit_text("Есть ли у вас аллергия?", reply_markup=yes_no_back_keyboard())
         await state.set_state(Questionnaire.allergies)
         return
     await state.update_data(format_product=query.data)
-    await query.message.edit_text("Теперь выберем аромат продукта", reply_markup=aromat_kb())
+
+    # Создаем медиагруппу с фотографиями
+    media_group = [
+        InputMediaPhoto(media=FSInputFile("color_1.jpg"), caption="Цвет 01"),
+        InputMediaPhoto(media=FSInputFile("color_2.jpg"), caption="Цвет 02"),
+        InputMediaPhoto(media=FSInputFile("color_3.jpg"), caption="Цвет 03")
+    ]
+
+    # Отправляем медиагруппу
+    await query.message.answer_media_group(media=media_group)
+
+    # Отправляем сообщение с выбором цвета
+    await query.message.answer("Теперь выберете цвет продукта", reply_markup=color_keyboard())
+    await state.set_state(Questionnaire.color_product)
+
+
+@router.callback_query(Questionnaire.color_product)
+async def process_color_product(query: CallbackQuery, state: FSMContext):
+    if query.data == "back":
+        # Возвращаем пользователя к выбору формата продукта
+        await query.message.edit_text("Теперь выберем формат продукта", reply_markup=format_product_kb())
+        await state.set_state(Questionnaire.format_product)
+        return
+
+    await state.update_data(color_product=query.data)
+
+    # Переход к выбору аромата
+    await query.message.answer("Теперь выберем аромат продукта", reply_markup=aromat_kb())
     await state.set_state(Questionnaire.aromat)
 
 
 @router.callback_query(Questionnaire.aromat)
-async def process_allergies(query: CallbackQuery, state: FSMContext):
+async def process_aromat(query: CallbackQuery, state: FSMContext):
     if query.data == "back":
-        await query.message.edit_text("Теперь выберем формат продукта", reply_markup=format_product_kb())
-        await state.set_state(Questionnaire.format_product)
+        # Удаляем предыдущее сообщение (если нужно)
+        await query.message.delete()
+
+        # Создаем медиагруппу с фотографиями
+        media_group = [
+            InputMediaPhoto(media=FSInputFile("color_1.jpg"), caption="Цвет 01"),
+            InputMediaPhoto(media=FSInputFile("color_2.jpg"), caption="Цвет 02"),
+            InputMediaPhoto(media=FSInputFile("color_3.jpg"), caption="Цвет 03")
+        ]
+
+        # Отправляем медиагруппу
+        await query.message.answer_media_group(media=media_group)
+
+        # Отправляем сообщение с выбором цвета
+        await query.message.answer("Теперь выберете цвет продукта", reply_markup=color_keyboard())
+        await state.set_state(Questionnaire.color_product)
         return
+
     await state.update_data(aromat=query.data)
     data = await state.get_data()
     user_name = query.from_user.full_name
     user_id = query.from_user.username if query.from_user.username else f"ID: {query.from_user.id}"
+
     # Преобразуем ответы в читаемый формат
     responses = {
         "dry_lips": "Сухость губ",
@@ -172,14 +235,13 @@ async def process_allergies(query: CallbackQuery, state: FSMContext):
         "smoke": "Курение",
         "allergies": "Аллергии",
         "format_product": "Формат продукта",
+        "color_product": "Цвет продукта",
         "aromat": "Аромат"
     }
     formatted_responses = []
     for key, value in data.items():
-        question = responses.get(key, key)# Получаем понятный текст вопроса
-        answer = "Да"
-        if value == "no":
-            answer = "Нет"
+        question = responses.get(key, key)
+        answer = "Да" if value == "yes" else "Нет"
         if value == "f_cherry":
             answer = "Вишня"
         if value == "f_plombir":
@@ -196,6 +258,12 @@ async def process_allergies(query: CallbackQuery, state: FSMContext):
             answer = "Бальзам в стике"
         if value == "f_mask_for_lips":
             answer = "Маска для губ"
+        if value == "color_01":
+            answer = "01"
+        if value == "color_02":
+            answer = "02"
+        if value == "color_03":
+            answer = "03"
         formatted_responses.append(f"{question}: {answer}")
 
     # Собираем итоговое сообщение
@@ -207,9 +275,7 @@ async def process_allergies(query: CallbackQuery, state: FSMContext):
     await state.clear()
     # Отправка данных в определенный чат
     await query.bot.send_message(chat_id=-4612151315, text=response_message)
-
-    # Состояния для выбора готового продукта
-
+# Состояния для выбора готового продукта
 
 
 from aiogram import types
@@ -217,16 +283,19 @@ from aiogram.fsm.context import FSMContext
 from aiogram.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
 class RProduct(StatesGroup):
     category_state = State()
     product_state = State()
     count = State()
+
 
 # Хэндлер для выбора готового продукта
 @router.callback_query(lambda query: query.data == "ready_product")
 async def choose_product_type(query: CallbackQuery, state: FSMContext):
     await query.message.edit_text("Выберите формат продукта", reply_markup=main_keyboard())
     await state.set_state(RProduct.category_state)
+
 
 # Хэндлер для обработки выбора категории
 @router.callback_query(RProduct.category_state)
@@ -274,6 +343,7 @@ async def handle_category_selection(query: CallbackQuery, state: FSMContext, ses
     await query.message.answer("Что хотите заказать?", reply_markup=products_kb)
     await state.set_state(RProduct.product_state)
 
+
 # Хэндлер для обработки выбора товара
 @router.callback_query(RProduct.product_state)
 async def handle_product_selection(query: CallbackQuery, state: FSMContext):
@@ -291,7 +361,6 @@ async def handle_product_selection(query: CallbackQuery, state: FSMContext):
     await query.message.delete()
     await query.message.answer("Укажите количество")
     await state.set_state(RProduct.count)
-
 
 
 # Хэндлер для ввода количества
@@ -333,4 +402,6 @@ async def handle_count_input(message: types.Message, state: FSMContext):
     )
 
     await state.clear()
-    await message.answer("Ваш заказ успешно отправлен!\nСкоро с вами свяжеться менеджер\nПо всем вопросам писать - @itskkira или @splvll\nЖелаете ли заказать что-ниубудь еще? ",reply_markup=start_keyboard())
+    await message.answer(
+        "Ваш заказ успешно отправлен!\nСкоро с вами свяжеться менеджер\nПо всем вопросам писать - @itskkira или @splvll\nЖелаете ли заказать что-ниубудь еще? ",
+        reply_markup=start_keyboard())
